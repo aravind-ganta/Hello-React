52bb94dc5491de2256a45a184b78209d2f34bc5a
Aravind Ganta <aravind.ganta@sadhguru.org>
2024-09-20 17:50:49 +0530

    Igniting The App

--- App.js
+++ App.js
@@ -1,4 +1,30 @@
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1!");
+/**
+ * HMR- Hot Module Replacement
+ * File Watcher Algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our code
+ * Dev and Production build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev
+ * port number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ *
+ * Transitive Dependencies
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement(
+  "h1",
+  { id: "title" },
+  "Heading 1 from Parcel!"
+);
 const heading2 = React.createElement("h2", { id: "title" }, "Heading 2!");
 const container = React.createElement("div", { id: "container" }, [
   heading1,
