bce385849d5008947b9652eef7c69c18462e7ada
Aravind Ganta <gantaaravind1196@gmail.com>
2024-09-29 22:53:10 +0530

    Class Based Components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/Restaurantmenu";
+import Profile from "./components/ProfileClass";
 
 // React.Fragment
 const AppLayout = () => {
@@ -33,6 +34,12 @@ const appRouter=createBrowserRouter([
       {
         path: "/about",
         element: <About/>,
+        children:[
+          {
+            path:"profile",
+            element:<Profile/>,
+          }
+        ]
       },
       {
         path: "/contact",
